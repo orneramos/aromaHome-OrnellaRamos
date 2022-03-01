006ee4ec948427a1a3012cb9b83114cd13b7c14e
orneramos <ornellaramos.y@gmail.com>
2022-03-01 18:36:47 -0300

    agregar cartwidget icon y estilo navbar

---
+++ src/componentes/CartWidget.js
@@ -0,0 +1,11 @@
+import { FaShoppingCart } from 'react-icons/fa';
+
+const CartWidget = () => {
+    return(
+        <div>
+            <FaShoppingCart />
+        </div>
+    )
+}
+
+export default CartWidget
--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,17 +1,20 @@
 import { Navbar as BootstrapNavBar, Nav, Container } from 'react-bootstrap';
+import CartWidget from './CartWidget';
+import './Navbar.css';
 
 const Navbar = () => {
     return(
         <div>
                 <BootstrapNavBar bg="light" expand="lg">
                     <Container>
-                    <BootstrapNavBar.Brand href="#home">Aroma-Home</BootstrapNavBar.Brand>
+                    <BootstrapNavBar.Brand href="#home">Aroma</BootstrapNavBar.Brand>
                     <BootstrapNavBar.Toggle aria-controls="basic-navbar-nav" />
                     <BootstrapNavBar.Collapse className="justify-content-end" id="basic-navbar-nav">
-                    <Nav>
+                    <Nav className="Navbar-brand">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#productos">Productos</Nav.Link>
                         <Nav.Link href="#contacto">Contacto</Nav.Link>
+                        <Nav.Link href="#home"><CartWidget /></Nav.Link>
                     </Nav>
                     </BootstrapNavBar.Collapse>
                     </Container>
