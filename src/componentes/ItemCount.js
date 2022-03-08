f4921ca183292f09526b41418eb168d1fd1f4916
orneramos <ornellaramos.y@gmail.com>
2022-03-08 13:42:45 -0300

    eliminar disabled

--- src/componentes/ItemCount.js
+++ src/componentes/ItemCount.js
@@ -3,19 +3,15 @@ import { useState } from "react"
 
 const ItemCount = ({inicial, stock}) => {
     const [cantidad, setCantidad] = useState(inicial)
-    const [disabled, setDisabled] = useState(false)
     const agregar = () => {
         console.log(`${cantidad} unidades del producto elegido fueron agregadas al carrito`)
     }
     const sumar = () => {
         if(cantidad < stock) {
             setCantidad(cantidad + 1) 
-        } else {
-            setDisabled(true)
-        }
+        } 
     }
     const restar = () => {
-        setDisabled(false)
         if(cantidad > 1) {
             setCantidad(cantidad - 1)
         }
@@ -28,7 +24,7 @@ const ItemCount = ({inicial, stock}) => {
                 <span>{cantidad}</span>
                 <Button variant="light" onClick={sumar}>+</Button>{' '}
             </div>
-            <Button variant="secondary" onClick={agregar} disabled={disabled}>Agregar al carrito</Button>{' '}
+            <Button variant="secondary" onClick={agregar}>Agregar al carrito</Button>{' '}
         </>
     )
 }
