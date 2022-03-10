ac8dead73c693a18e524a5c3188e8915eccabd04
orneramos <ornellaramos.y@gmail.com>
2022-03-10 20:36:16 -0300

    modificar ItemListContainer, crear Promise para cargar productos

--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -1,8 +1,32 @@
 import ItemList from "./ItemList";
 
 const ItemListContainer = () => {
+    const productos = [
+        {
+            tipoProducto: "Difusor",
+            fragancia: "Sweet Dreams",
+            precio: 600,
+            imgURL:"../imagenes/difusor.jpg"
+        },
+        {
+            tipoProducto: "Difusor",
+            fragancia: "Sandalo dulce",
+            precio: 600,
+            imgURL:"../imagenes/difusor.jpg"
+        }
+    ]
+    const cargarProductos = new Promise((resolve, reject)=> {
+        setTimeout(() => {
+            resolve("¡Éxito!" + productos);
+        }, 2000);
+    })
+    cargarProductos.then(result => {
+        console.log(result)
+    }).catch((err)=>{
+        console.log("No se pudieron cargar los productos" + err)
+    })
     return (
-        <ItemList />
+        <ItemList productos={productos}/>
     )
 }
 
