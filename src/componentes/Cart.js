3784f55a6851eccf264572486390967c4861ced8
orneramos <ornellaramos.y@gmail.com>
2022-03-27 21:11:41 -0300

    agregar estilo al cart

--- src/componentes/Cart.js
+++ src/componentes/Cart.js
@@ -7,14 +7,20 @@ const Cart = () => {
     const carritoContext = useContext(CartContext)
 
     return(
-        <div className="container">
-            { 
-                carritoContext.productosEnCarrito.map((producto) => {
-                    return <CartItem key={producto.item.id} itemProps={producto}/> 
-                })
-            }
-            <div className="m-5 d-flex justify-content-around">
+        <div className="container mt-5">
+            <h2>Tu carrito</h2>
+            <ul className="list-group">
+                { 
+                    carritoContext.productosEnCarrito.map((producto) => {
+                        return <CartItem key={producto.item.id} itemProps={producto}/> 
+                    })
+                }
+            </ul>
+            <div className="justify-content-end d-flex mt-2">
                 <Button variant="outline-danger" onClick={carritoContext.clear}>Vaciar carrito</Button>
+            </div>
+            <div className="m-5 d-flex justify-content-around">
+                <h3>Total: {}</h3>
                 <Button variant="success">Finalizar compra</Button>
             </div>
         </div>
--- src/componentes/CartItem.js
+++ src/componentes/CartItem.js
@@ -7,9 +7,12 @@ export const CartItem = ({itemProps}) => {
     const carritoContext = useContext(CartContext)
     return(
         <div>
-            <h1>Item {itemProps.item.nombre} {itemProps.item.fragancia}</h1>
-            <p>{itemProps.quantity} unidades X $ {itemProps.item.precio}</p>
-            <Button variant="outline-danger" onClick={()=>carritoContext.removeItem(itemProps.item.id)}> <BsTrash /> </Button>
+            <li className="list-group-item d-flex justify-content-between">
+                <h3>Item {itemProps.item.nombre} {itemProps.item.fragancia}</h3>
+                <p>{itemProps.quantity} unidades</p>
+                <p>$ {itemProps.quantity * itemProps.item.precio}</p>
+                <Button variant="outline-danger" onClick={()=>carritoContext.removeItem(itemProps.item.id)}> <BsTrash /> </Button>
+            </li>
         </div>
-    )
+    )   
 }
