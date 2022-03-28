18543bbba547b654ad40130f7dbd673c7929a85f
orneramos <ornellaramos.y@gmail.com>
2022-03-28 19:18:08 -0300

    modificar cartwidget para no mostrar si el carrito esta vacio

--- src/componentes/CartWidget.js
+++ src/componentes/CartWidget.js
@@ -1,11 +1,13 @@
-import { Button } from 'bootstrap';
 import { useContext } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
 import { CartContext } from '../context/CartContext';
 
 const CartWidget = (totalItems) => {
+
     const carritoContext = useContext(CartContext)
-    return(
+
+    return (
+        carritoContext.productosEnCarrito.length > 0 &&
         <div>
            <FaShoppingCart />
             <span className='m-1'>{carritoContext.totalItems}</span>
